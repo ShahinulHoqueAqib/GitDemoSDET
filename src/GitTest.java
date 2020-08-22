4c62f7e9d261e88d2a134780e74e758af0ff9e4a
ShahinulHoqueAqib <shahin.aqib@gmail.com>
2020-08-22 21:49:26 +0600

    First Commit

---
+++ src/GitTest.java
@@ -0,0 +1,12 @@
+public class GitTest 
+{
+	public static void main(String[] args)
+	{
+		String hungry = "Yes";
+		if (hungry == "N")
+		{
+			System.out.println("Eat Something...");
+		}
+		System.out.println("DO work...");
+	}
+}
